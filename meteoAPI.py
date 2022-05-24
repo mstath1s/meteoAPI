3b91871dbfa7013d62484c0b2998faf1b3751d58
John Papagiannakos <jpapagiannakos@gmail.com>
2022-05-24 14:00:34 +0300

    feat: first stable bersion

--- meteoAPI.py
+++ meteoAPI.py
@@ -1,5 +1,7 @@
+from time import time
 import requests
 import re
+from datetime import date, datetime, time, timedelta
 
 from headers import headers
 
@@ -14,35 +16,40 @@ def filterResultSet2list(input, keep, remove_l='', remove_r=''):
             filtered = filtered.replace(remove_l,'')
             filtered = filtered.replace(remove_r,'')
             list.append(filtered)
+        else:
+            list.append(0)
     
     return list
 
+def meteogrGetFirstHour(soup):
+    source_hours = soup.find_all("td", class_= re.compile("innerTableCell fulltime"))
+    
+    starting_hour = filterResultSet2list(source_hours[0], '\d\d:\d\d', '', ':\d\d')
+    # print(starting_hour[0])
+    time_init = starting_hour[0] + ':00'
+    time_init = datetime.strptime(time_init,"%X").time()
+    print(time_init)
 
-def meteogrGetAllHumidity(soup):
-    source_humidity = soup.find_all("td", class_= re.compile("humidity"))
-
-    source_humidity_list = filterResultSet2list(source_humidity, '\d\d\d*')
-
-    return source_humidity_list
+    return time_init
 
 def meteogrGetAllTemperature(soup):
-    source_celcius = soup.find_all("td", class_= re.compile("innerTableCell temperature normal tempwidth"))
+    source_celcius = soup.find_all("td", class_= re.compile("innerTableCell temperature"))
     
-    source_celcius_list = filterResultSet2list(source_celcius, '>\d\d\d*<', '<', '>')
+    source_celcius_list = filterResultSet2list(source_celcius, '>\d*<', '<', '>')
     
     return source_celcius_list
 
-def meteogrGetAllHours(soup):
-    source_hours = soup.find_all("td", class_= re.compile("innerTableCell fulltime"))
-    
-    source_hours_list = filterResultSet2list(source_hours, '\d\d:\d\d')
-    
-    return source_hours_list    
+def meteogrGetAllHumidity(soup):
+    source_humidity = soup.find_all("td", class_= re.compile("innerTableCell hidden-xs"))
+
+    source_humidity_list = filterResultSet2list(source_humidity, 'humidity\">\d*\%','humidity\">','\%')
+
+    return source_humidity_list
 
 def meteogrGetAllWindspeeds(soup):
-    source_ws = soup.find_all("span", class_= re.compile("kmh"))
+    source_ws = soup.find_all("td", class_= re.compile("innerTableCell anemosfull"))
     
-    source_ws_list = filterResultSet2list(source_ws, '\d\d* Km/h', '', 'Km/h')
+    source_ws_list = filterResultSet2list(source_ws, '\d* Km/h', '', 'Km/h')
 
     return source_ws_list
 
@@ -58,9 +65,41 @@ def meteoGetTuple(url):
 
     temp = meteogrGetAllTemperature(soup)
 
-    hours = meteogrGetAllHours(soup)
+    time_init = meteogrGetFirstHour(soup)
 
     ws = meteogrGetAllWindspeeds(soup)
+
+    hours=[]
+    time = datetime.combine(date.today(), time_init)
+
+    time_change = timedelta(hours=3)
+
+    for i in range(len(temp)):
+        hours.append(time)
+        time = time + time_change
+
+    ###
+    # print('Temperatures')
+    # for row in temp:
+    #     print(row)
+
+    # print('Humidity ')
+    # for row in humidity:
+    #     print(row)
+    
+    # print('Hours ')
+    # for row in hours:
+    #     print(row)
+
+    # print('Windspeed')
+    # for row in ws:
+    #     print(row)
+
+    # print("\n")
+    # print(len(hours))
+    # print(len(temp))
+    # print(len(humidity))
+    # print(len(ws))
 
     joined_data = list(zip(hours, temp, humidity, ws))
 
