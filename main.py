77d6add1229bf05c402abecfffb7087ed530c1d2
John Papagiannakos <jpapagiannakos@gmail.com>
2022-05-24 15:01:55 +0300

    feat: test 2 locations successfully

--- main.py
+++ main.py
@@ -7,4 +7,13 @@ from bs4 import BeautifulSoup, ResultSet
 if __name__ == "__main__":
 
     url = 'https://www.meteo.gr/cf-en.cfm?city_id=310'
-    meteoGetTuple(url)
+    
+    location, hours, temp, humidity, ws = meteogrGetTuple(url)
+    
+    meteogrPlotTuple(location, hours, temp, humidity, ws)
+
+    url = 'https://www.meteo.gr/cf-en.cfm?city_id=191'
+
+    location, hours, temp, humidity, ws = meteogrGetTuple(url)
+
+    meteogrPlotTuple(location, hours, temp, humidity, ws)
