9392b06e2783f945d5f8c4a971c8391fb1a4a572
John Papagiannakos <jpapagiannakos@gmail.com>
2022-05-25 18:00:11 +0300

    feat: added METEOGR_DAYTIME_FORMAT

--- consts.py
+++ consts.py
@@ -1,3 +1,5 @@
 METEOGR_TIME_PERIOD_HOURS = 3
 
+METEOGR_DAYTIME_FORMAT = "%m/%d/%Y, %H:%M:%S"
+
 WIND_DIRECTION = ['N', 'NNE', 'NE', 'ENE', 'E', 'ESE', 'SE', 'SSE', 'S', 'SSW', 'SW', 'WSW', 'W', 'WNW', 'NW', 'NNW']
