0dd0c4c268082ab903cf38e37e7749b6cd786fae
John Papagiannakos <jpapagiannakos@gmail.com>
2022-05-25 15:48:59 +0300

    refactor: moved all utility functions from meteoAPI.py to utils.py

---
+++ utils.py
@@ -0,0 +1,42 @@
+from consts import *
+
+
+def filterResultSet2list(input, keep, remove_l='', remove_r=''):
+    list = []
+    for item in input:
+        filtered = re.search(keep, str(item))
+        if filtered:
+            filtered = filtered.group(0)
+            filtered = re.sub(remove_l, '', filtered)
+            filtered = re.sub(remove_r, '', filtered)
+            list.append(filtered)
+        else:
+            list.append(0)
+
+    return list
+
+
+def listStr2Flt(list_of_strings):
+    list_of_float = []
+    for item in list_of_strings:
+        list_of_float.append(float(item))
+    return list_of_float
+
+
+def plot2D(x, y, xlabel='', ylabel='', title=''):
+    plt.plot(x, y)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.title(title)
+    plt.show()
+    
+
+def WindDirTxt2Deg(wd):
+    wd_deg = []
+    for wd_txt in wd:
+        if wd_txt in WIND_DIRECTION:
+            wd_deg.append(WIND_DIRECTION.index(wd_txt) * 22.5)
+        else:
+            wd_deg.append(0)
+
+    return wd_deg
