64047e2a061fc845e9d99e8bc275c37134df49a1
John Papagiannakos <jpapagiannakos@gmail.com>
2022-05-25 18:21:58 +0300

    refactor: renamed METEOGR_DAYTIME_FORMAT -> METEOGR_DAYTIME_FORMAT_CSV, added METEOGR_DAYTIME_FORMAT_FNAME

--- consts.py
+++ consts.py
@@ -1,5 +1,7 @@
 METEOGR_TIME_PERIOD_HOURS = 3
 
-METEOGR_DAYTIME_FORMAT = "%m/%d/%Y--%H:%M:%S"
+METEOGR_DAYTIME_FORMAT_CSV = "%d/%m/%Y--%H:%M:%S"
+
+METEOGR_DAYTIME_FORMAT_FNAME = "%Y%m%d%H%M%S"
 
 WIND_DIRECTION = ['N', 'NNE', 'NE', 'ENE', 'E', 'ESE', 'SE', 'SSE', 'S', 'SSW', 'SW', 'WSW', 'W', 'WNW', 'NW', 'NNW']
