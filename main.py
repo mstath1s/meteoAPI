9099da7dd799ef0a353abbcbbf5d235d994e03dc
John Papagiannakos <jpapagiannakos@gmail.com>
2022-05-26 12:29:24 +0300

    feat: added func meteogrSaveAllDataCSV()

--- main.py
+++ main.py
@@ -4,26 +4,22 @@ if __name__ == "__main__":
     ### *** TEST 1 ***
     url = 'https://www.meteo.gr/cf-en.cfm?city_id=310' # Markoupoulo, Attica
     
-    location, hours, temp, humidity, ws, wd, skyCondition = meteogrGetTuple(url)
+    # location, hours, temp, humidity, ws, wd, skyCondition = meteogrGetTuple(url)
     
-    # meteogrPlotTuple(location, hours, temp, humidity)
-
-    meteogrPlotWindrose(ws, wd)
+    # meteogrPlotTuple(location, hours, temp, humidity, ws, wd)
+    
+    meteogrSaveAllDataCSV(url)
 
     ### *** TEST 2 ***
     url = 'https://www.meteo.gr/cf-en.cfm?city_id=191' # El. Venizelos, Athens airport
 
     location, hours, temp, humidity, ws, wd, skyCondition = meteogrGetTuple(url)
 
-    meteogrPlotWindrose(ws, wd)
-
-    # meteogrPlotTuple(location, hours, temp, humidity)
+    meteogrPlotTuple(location, hours, temp, humidity, ws, wd)
 
     ### *** TEST 3 ***
     url = 'https://www.meteo.gr/cf-en.cfm?city_id=20' # Santorini, Cyclades
 
     location, hours, temp, humidity, ws, wd, skyCondition = meteogrGetTuple(url)
 
-    meteogrPlotWindrose(ws, wd)
-
-    # meteogrPlotTuple(location, hours, temp, humidity)
+    meteogrPlotTuple(location, hours, temp, humidity, ws, wd)
