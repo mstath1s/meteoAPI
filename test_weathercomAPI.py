8e81371922dd41835d21b649537dd47a420f629d
John Papagiannakos <jpapagiannakos@gmail.com>
2022-06-03 14:33:08 +0300

    refactor: updated test file

--- test_weathercomAPI.py
+++ test_weathercomAPI.py
@@ -2,7 +2,15 @@ from weathercomAPI import *
 
 if __name__ == "__main__":
     ### *** TEST 1 ***
-    # Markoupoulo, Attica
+    # Markoupoulo, Attica, Greece
     url = 'https://weather.com/weather/hourbyhour/l/21e963c34293ccc9c399128b3c25d32afd754dec039c0376a7c974185584f29d#detailIndex4'
-    
-    weathercomGetTuple(url)
+
+    location, time, temp, humidity, ws, wd, skyCondition, source_cloudCover, uv_idx, rain_amount = weathercomGetTuple(url)
+    weathercomPlotTuple(location, time, temp, humidity, ws, wd)
+
+    # weathercomSaveAllDataCSV(url)
+
+    # Kaunas, Kaunas, Lithuania
+    url = 'https://weather.com/weather/hourbyhour/l/c0ad3acc0ae559d552ded89fb4af50e09b2063ef1ffa2bb7305acf6083011ea4'
+
+    weathercomSaveAllDataCSV(url)
